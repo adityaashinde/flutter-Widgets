a05e33bf2f0e18ccfc968bf42533b0afe3f75d77
adityaashinde <adityashinde4580@gmail.com>
2024-06-27 21:41:16 +0530

    update

--- widget_piechart/lib/main.dart
+++ widget_piechart/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,41 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: PieChartDemo(),
+    );
+  }
+}
+
+class PieChartDemo extends StatefulWidget {
+  const PieChartDemo({super.key});
+
+  @override
+  State createState() => _PieStateDemo();
+}
+
+class _PieStateDemo extends State {
+  Map<String, double> data = {
+    "Food": 20,
+    "Movies": 10,
+    "Entertainment": 40,
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("Pie Chart Demo"),
+      ),
+      body: Center(
+        child: PieChart(
+          dataMap: data,
+          animationDuration: const Duration(milliseconds: 2000),
+          chartType: ChartType.ring,
+          chartRadius: 200,
+          ringStrokeWidth: 100,
+          centerText: "Total",
         ),
       ),
     );
--- widget_stack/lib/main.dart
+++ widget_stack/lib/main.dart
@@ -10,11 +10,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Widget Stack!'),
+        home: Scaffold(
+          body: Center(
+            child: Text('Widget Stack!'),
+          ),
         ),
-      ),s
-    );
+        s);
   }
 }
